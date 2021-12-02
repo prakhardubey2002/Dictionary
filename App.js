c210ab3185f10c61d2e951b2844dbf3982c70544
Prakhar Dubey <80157067+prakhardubey2002@users.noreply.github.com>
2021-12-03 01:38:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 let input =document.querySelector('#input');
 let searchBtn =document.querySelector('#search');
+let notFound = document.querySelector('.not_found');
+let defbox = document.querySelector('.def');
 searchBtn.addEventListener('click',function(e){
     e.preventDefault();
     let word= input.value;
     if(word===''){
-        aleert('word required');
+        alert('word required');
         return;
     }
     getData(word);
 })
-function getData{
-    
+async function getData(word){
+    const responses = await fetch(`https://dictionaryapi.com/api/v3/references/learners/json/${word}?key=a7822f8c-46c7-43fe-aa18-f73023edf902`);
+
+    const data = await responses.json();
+    console.log(data);
+    //if empty result
+    if(!data.length){
+        notFound.innerText = 'No result found you are typing gibberish maybe!'
+        return;
+    }
+    //if result is suggestions
+    if(typeof data[0] === 'string'){
+         let heading = document.createElement('h3');
+         heading.innerText = "Did you mean?"
+         notFound.appendChild(heading);
+         data.forEach(element => {
+             let suggestion = document.createElement('span');
+             suggestion.classList.add('suggested');
+             suggestion.innerText = element;
+             notFound.appendChild(suggestion)
+         })
+         return;
+    }
+    // result found
+    let defination = data[0].shortdef[0];
+    defbox.innerHTML= defination;
 }
